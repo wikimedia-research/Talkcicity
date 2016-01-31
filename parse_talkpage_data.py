9d41fa87e396eca3ffd39d16c3f73ba59ab94a32
ironholds <ironholds@gmail.com>
2016-01-31 15:50:15 -0500

    split out into a class

--- parse_talkpage_data.py
+++ parse_talkpage_data.py
@@ -1,43 +1,12 @@
 # Python 3 please.
-from wikichatter import talkpageparser
-from os import listdir
 import random
-import re
-import mwparserfromhell as mwp
-import pickle
-
-def extract_sections(page_text):
-  for section in page_text['sections']:
-    section = extract_comments(section)
-  return(page_text)
-
-# Once we've got an individual section, this goes through each comment getting the text
-def extract_comments(section):
-  for comment in section['comments']:
-    comment['text_blocks'] = extract_text(comment['text_blocks'])
-  return(section)
-
-# Get the actual comment text :/
-def extract_text(comment):
-  comment = re.sub("\\[\\[(File|Image):.*\\]\\]", "", '\n'.join(comment)) # File and image links go wonky when mwp handles it.
-  comment = re.sub("<article id=.*\\n\\t<talkpage.*>", "", comment) # Merge and remove content metadata
-  comment = mwp.parse(comment).strip_code(normalize=True, collapse=True)
-  comment = re.sub("(<!--.*-->|</article>|</talkpage>)", "", comment)
-  return(comment)
+from os import listdir
+import talk_collection
 
 # Get the wikitext files, subset
-files = random.sample(listdir("./data/talk_pages"), 100)
+dir = "./data/talk_pages/"
+files = [dir + filestring for filestring in random.sample(listdir(dir), 100)]
 
-output = list()
-for file in files:
-  f_path = "./data/talk_pages/" + file;
-  with open(f_path, "r") as f:
-    text = f.read()
-    try:
-      parsed = talkpageparser.parse(text)
-      output.append(extract_sections(parsed))
-    except:
-      pass
-
-with open("parsed_sample.pickle", "wb") as file:
-  pickle.dump(output, file)
+# Generate parsed versions and write out
+parsed_versions = talk_collection.talk_collection(files)
+parsed_versions.write_comments("parsed_sample.pickle")
---
+++ talk_collection.py
@@ -0,0 +1,54 @@
+# Python 3 please.
+from wikichatter import talkpageparser
+import re
+import mwparserfromhell as mwp
+import pickle
+import warnings
+
+class talk_collection:
+  
+  # Write comments out
+  def write_comments(self, file):
+    with open("parsed_sample.pickle", "wb") as file:
+      pickle.dump(self.talk_entries, file)
+  
+  # Extract sections from the page
+  def extract_sections(page_text):
+    for section in page_text['sections']:
+      section = extract_comments(section)
+    return(page_text)
+  
+  # Once we've got an individual section, this goes through each comment getting the text
+  def extract_comments(section):
+    for comment in section['comments']:
+      comment['text_blocks'] = parse_text(comment['text_blocks'])
+    return(section)
+  
+  # Get the actual comment text :/
+  def parse_text(comment):
+    comment = re.sub("\\[\\[(File|Image):.*\\]\\]", "", '\n'.join(comment)) # File and image links go wonky when mwp handles it.
+    comment = re.sub("<article id=.*\\n\\t<talkpage.*>", "", comment) # Merge and remove content metadata
+    comment = mwp.parse(comment).strip_code(normalize=True, collapse=True)
+    comment = re.sub("(<!--.*-->|</article>|</talkpage>)", "", comment)
+    return(comment)
+  
+  # The constructor. Takes in a list of files, parses and cleans the comments and stores
+  # the resulting structures in the talk_collection object.
+  def __init__(self, files, warn = False):
+    
+    self.talk_entries = list()
+    self.failed_files = list()
+    
+    for file in files:
+      with open(file, "r") as f:
+        text = f.read()
+      try:
+        parsed = talkpageparser.parse(text)
+        self.talk_entries.append(extract_sections(parsed))
+      except:
+        self.failed_files.append(file)
+      
+    if warn is True:
+      if len(self.failed_files) > 0:
+        warnings.warn("Some files failed to be read and parsed. See the failed_files list in the talk_collection object")
+        
